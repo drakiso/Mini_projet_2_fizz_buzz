44f9495bf026aa81a104f205719d8cae562d9b6f
Dr AKIso <anzankisrael@yahoo.com>
2023-06-04 01:20:13 +0300

    add modifications, the code is more on PEP8 standarts now

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,11 @@
 """A childhood counting game called Fizz Buzz"""
 
 for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0 :
+    if number % 3 == 0 and number % 5 == 0:
         print("Fizz Buzz")
-    elif number % 3 == 0 :
+    elif number % 3 == 0:
         print("Fizz")
-    elif number % 5 == 0 :
+    elif number % 5 == 0:
         print("Buzz")
     else:
         print(number)
